ff4f801ca0ee6d87b0e69e4192f2fdf8e58a5fd0
Austin Nguyen <123275620+AustinNguyen16@users.noreply.github.com>
2023-04-18 11:00:08 -0500

    Complete

--- main.py
+++ main.py
@@ -5,25 +5,65 @@ test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relev
 alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
-    else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+    m, n = len(S), len(T)
+    table = [[0] * (n + 1) for _ in range(m + 1)]
 
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                table[i][j] = j
+            elif j == 0:
+                table[i][j] = i 
+            elif S[i-1] == T[j-1]:
+                table[i][j] = table[i-1][j-1]
+            else:
+                table[i][j] = 1 + min(table[i][j-1], table[i-1][j], table[i-1][j-1])
+    return table[m][n]
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement memoization
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)]
+    if (T, S) in MED:
+        return MED[(T, S)]
+
+    if (S == ""):
+        result = len(T)
+    elif (T == ""):
+        result = len(S)
+    elif (S[0] == T[0]):
+        result = fast_MED(S[1:], T[1:], MED)
+    else:
+        result = 1 + min(fast_MED(S, T[1:], MED), 
+                         fast_MED(S[1:], T, MED),  
+                         fast_MED(S[1:], T[1:], MED))
+
+    MED[(S, T)] = result
+    return result
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    if (S == ""):
+        return ('-' * len(T), T)
+    elif (T == ""):
+        return (S, '-' * len(S))
+    elif (S[0] == T[0]):
+        align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+        return (S[0] + align_S, T[0] + align_T)
+    else:
+        replace = fast_MED(S[1:], T[1:], MED)
+        insert = fast_MED(S, T[1:], MED)
+        remove = fast_MED(S[1:], T, MED)
+
+        min_val = min(replace, insert, remove)
+
+        if min_val == replace:
+            align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+            return (S[0] + align_S, T[0] + align_T)
+        elif min_val == insert:
+            align_S, align_T = fast_align_MED(S, T[1:], MED)
+            return ('-' + align_S, T[0] + align_T)
+        else:
+            align_S, align_T = fast_align_MED(S[1:], T, MED)
+            return (S[0] + align_S, '-' + align_T)
 
 def test_MED():
     for S, T in test_cases:
@@ -34,3 +74,12 @@ def test_align():
         S, T = test_cases[i]
         align_S, align_T = fast_align_MED(S, T)
         assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+
+#for i in range(len(test_cases)):
+ #       S, T = test_cases[i]
+  #    print(align_S)
+    #    print(alignments[i][0])
+   #     print(align_T)
+    #    print(alignments[i][1])
+  #
+
